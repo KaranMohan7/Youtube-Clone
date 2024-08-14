e82f368660a89f02f0fcf44396b36235f7cb049a
Karan <karanmohan44@gmail.com>
2024-08-14 14:06:15 +0530

    bug fixes

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loading = () => {
   return (
   
-    <div className='grid lg:ml-[460px] xl:[410px] min-h-screen'>
+    <div className='grid lg:ml-[400px] xl:[410px] min-h-screen'>
 <div class="  border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-red-600 place-self-center" />
     </div>
   )
